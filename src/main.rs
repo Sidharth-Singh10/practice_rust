90766444c169e2c2439027d6a5f5e86e5c828650
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2024-05-04 14:21:36 +0530

    traits

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+trait Trait {
+    fn f(&self);
+}
 
+impl Trait for u32 {
+    fn f(&self) {
+        print!("1");
+    }
+}
 
-
+impl<'a> Trait for &'a i32 {
+    fn f(&self) {
+        print!("2");
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let x = &0; 
+    // x needs to inferred as &{something} argument x must be of type &Self for some type Self that implements Trait.
+    // We find that inferring 0: u32 satisfies both the constraint that u32 is an integer as well as u32 implements Trait, 
+    //so the method call ends up calling <u32 as Trait>::f(x) and prints 1.
+    x.f();
 }
